544126d0b3ede941b91527d0a9b736e8e3f48ad7
Lane Tung <continue2015@outlook.com>
2022-11-28 21:35:41 +0800

    Init Repo

---
+++ src/main/java/com/crowdos/backend/dataEntities/user.java
@@ -0,0 +1,10 @@
+package com.crowdos.backend.dataEntities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import org.springframework.boot.SpringApplication;
+@Table(name = "user")
+class user {
+
+
+}
---
+++ src/test/java/com/crowdos/backend/datainterface/DatainterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crowdos.backend.datainterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatainterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
