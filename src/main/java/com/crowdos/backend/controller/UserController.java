c74dac09e594b49a83deb8d54198f1e8dad2c3cc
AmashiroNoah <42802689+AmashiroNoah@users.noreply.github.com>
2022-12-22 20:25:15 +0800

    Update event.java

--- src/main/java/com/crowdos/backend/controller/UserController.java
+++ src/main/java/com/crowdos/backend/controller/UserController.java
@@ -2,6 +2,7 @@ package com.crowdos.backend.controller;
 
 
 import com.crowdos.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,7 +24,7 @@ public class UserController {
         else return userService.findUserById(uid).getName();
     }
 
-
+    @Autowired
     private UserService userService;
 
     @PostMapping("register")
